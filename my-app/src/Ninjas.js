3c36aad4177187dda2d6a86a05ca61dc60dbf4d4
Isuru Gunawardana <isuru.gnw@gmail.com>
2020-12-09 08:15:11 +0530

    tuto 16 - IF improved return

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -1,31 +1,6 @@
 import React from 'react';
 
 const Ninjas = ({ ninjas }) => {
-    // const ninjaList = ninjas.map(ninja => {
-    //     return ninja.age > 15 ?
-    //         (
-    //             <div className="ninja" key={ninja.id} >
-    //                 <div>Name: {ninja.name}</div>
-    //                 <div>Age : {ninja.age}</div>
-    //                 <div>Belt: {ninja.belt}</div>
-    //             </div >
-    //         )
-    //         :
-    //         null;
-    // })
-    // const ninjaList = ninjas.map(ninja => {
-    //     if (ninja.age > 15) {
-    //         return (
-    //             <div className="ninja" key={ninja.id} >
-    //                 <div>Name: {ninja.name}</div>
-    //                 <div>Age : {ninja.age}</div>
-    //                 <div>Belt: {ninja.belt}</div>
-    //             </div >
-    //         )
-    //     } else {
-    //         return null;
-    //     }
-    // })
     return (
         <div className="ninja-list" >
             {
