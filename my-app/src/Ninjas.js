d5d2d3cf86126500307606d5bc859097757a49d6
Isuru Gunawardana <isuru.gnw@gmail.com>
2020-12-09 07:37:33 +0530

    tuto 16 - IF improved return

--- my-app/src/Ninjas.js
+++ my-app/src/Ninjas.js
@@ -28,7 +28,20 @@ const Ninjas = ({ ninjas }) => {
     // })
     return (
         <div className="ninja-list" >
-            { ninjaList}
+            {
+                ninjas.map(ninja => {
+                    return ninja.age > 15 ?
+                        (
+                            <div className="ninja" key={ninja.id} >
+                                <div>Name: {ninja.name}</div>
+                                <div>Age : {ninja.age}</div>
+                                <div>Belt: {ninja.belt}</div>
+                            </div >
+                        )
+                        :
+                        null;
+                })
+            }
         </div>
     )
 }
