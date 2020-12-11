d6b0b4c5a5a2544e4fe2be594b5385b1d0229b1d
Isuru Gunawardana <isuru.gnw@gmail.com>
2020-12-11 08:40:34 +0530

    tuto 21 adding css

---
+++ my-app/src/AddNinja.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react'
+
+class AddNinja extends Component {
+    state = {
+        name: null,
+        age: null,
+        belt: null
+    }
+
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handldSubmit = (e) => {
+        e.preventDefault();
+        this.props.addNinja(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handldSubmit}>
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" id="name" onChange={this.handleChange} />
+                    <br /><label htmlFor="name">Age: </label>
+                    <input type="text" id="age" onChange={this.handleChange} />
+                    <br /><label htmlFor="name">Belt: </label>
+                    <input type="text" id="belt" onChange={this.handleChange} />
+                    <br /><button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default AddNinja;
